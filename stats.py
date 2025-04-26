e402ced01c2a8eaa372be6335174b48a6737d47f
Highlying <stuart.hendrickson@gmail.com>
2025-04-26 22:08:52 +1000

    Finished!

--- stats.py
+++ stats.py
@@ -1,19 +1,26 @@
-def get_word_count(text):
+def get_num_words(text):
     words = text.split()
     return len(words)
 
-def character_dict(text):
-    # Create a dictionary to store character counts
-    char_count = {}
-    
-    # Iterate through each character in the text
-    for char in text:
-        # If the character is not a space, add it to the dictionary
-        char = char.lower()
-        if char != " ":
-            if char in char_count:
-                char_count[char] += 1
-            else:
-                char_count[char] = 1
-    
-    return char_count
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
